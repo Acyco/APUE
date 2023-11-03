2af154687a60e904cf0935eab4668497256999d8
Acyco <chencongyi@live.com>
2023-11-04 04:07:20 +0800

    函数的嵌套调用

--- function/hello.c
+++ function/hello.c
@@ -2,16 +2,14 @@
 
 #include <stdio.h>
 
-// void print_hello(void); // 函数声明
+void print_hello(void); // 函数声明
 
-int main(void)
-{
-    print_hello();//在main函数要调用的函数，必须在main函数前先声明，不过现在不声明，编译器也会给你做链接上
-    return 0;
+int main(void) {
+  print_hello(); // 在main函数要调用的函数，必须在main函数前先声明，不过现在不声明，编译器也会给你做链接上,但依旧还会报警告
+  return 0;
 }
 
-
-void print_hello(void)//函数定义
+void print_hello(void) // 函数定义
 {
-    printf("Hello world!\n");
+  printf("Hello world!\n");
 }
