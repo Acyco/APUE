51fceea9615eb2c4884372f103d566de7a8711d9
Acyco <chencongyi@live.com>
2023-12-30 19:30:30 +0800

    APUE: exec函数族

---
+++ linux_c/process_basic/ex.c
@@ -0,0 +1,27 @@
+//
+// Created by Acyco on 2023-12-30.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * date +%s
+ */
+
+int main()
+{
+
+    puts("Begin!");
+
+    fflush(NULL); /*!!!*/
+
+    execl("/bin/date","date", "+%s", NULL);
+    perror("execl()");
+    exit(1);
+
+    puts("End!");
+
+    exit(0);
+}
